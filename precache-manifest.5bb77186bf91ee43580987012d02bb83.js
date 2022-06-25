6951fb1703e36b357f9e2ae50ac5e9c44b644cd6
Koushik099 <kou.saha.ami@gmail.com>
2022-06-25 19:31:19 +0530

    Updates

---
+++ precache-manifest.5bb77186bf91ee43580987012d02bb83.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1bc1242625585b4e09a6",
+    "url": "/Project/static/css/main.9ed70b12.chunk.css"
+  },
+  {
+    "revision": "1bc1242625585b4e09a6",
+    "url": "/Project/static/js/main.1bc12426.chunk.js"
+  },
+  {
+    "revision": "8f2f4211b0891106f089",
+    "url": "/Project/static/js/1.8f2f4211.chunk.js"
+  },
+  {
+    "revision": "e79e6ecb99fd60770733",
+    "url": "/Project/static/js/2.e79e6ecb.chunk.js"
+  },
+  {
+    "revision": "01d104716ac850346217",
+    "url": "/Project/static/js/runtime~main.01d10471.js"
+  },
+  {
+    "revision": "7b6877320f53916d51b56ce2bd7c3d03",
+    "url": "/Project/index.html"
+  }
+];
